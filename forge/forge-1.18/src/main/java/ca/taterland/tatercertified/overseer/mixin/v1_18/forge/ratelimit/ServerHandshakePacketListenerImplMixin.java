ef56fefffc59f3c6be36bf89b73d60602b9717a5
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-16 21:33:18 -0600

    Simplified the rejection handling a tad

--- forge/forge-1.18/src/main/java/ca/taterland/tatercertified/overseer/mixin/v1_18/forge/ratelimit/ServerHandshakePacketListenerImplMixin.java
+++ forge/forge-1.18/src/main/java/ca/taterland/tatercertified/overseer/mixin/v1_18/forge/ratelimit/ServerHandshakePacketListenerImplMixin.java
@@ -4,6 +4,8 @@
  */
 package ca.taterland.tatercertified.overseer.mixin.v1_18.forge.ratelimit;
 
+import static ca.taterland.tatercertified.overseer.mixin.v1_18.forge.ratelimit.RateLimitHelper.rejectConnection;
+
 import ca.taterland.tatercertified.overseer.v1_18.forge.OverseerForge;
 
 import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
@@ -13,15 +15,11 @@ import dev.neuralnexus.taterapi.MinecraftVersion;
 
 import net.minecraft.network.Connection;
 import net.minecraft.network.ConnectionProtocol;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
-import net.minecraft.network.protocol.login.ClientboundLoginDisconnectPacket;
 import net.minecraft.server.network.ServerHandshakePacketListenerImpl;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -33,20 +31,12 @@ public abstract class ServerHandshakePacketListenerImplMixin {
     @Shadow
     public abstract Connection shadow$getConnection();
 
-    @Unique private static final Component ddosfix$reason =
-            new TranslatableComponent("multiplayer.disconnect.unverified_username");
-
-    @Unique private static final ClientboundLoginDisconnectPacket ddosfix$disconnectPacket =
-            new ClientboundLoginDisconnectPacket(ddosfix$reason);
-
     @Inject(method = "handleIntention", at = @At("HEAD"), cancellable = true)
     public void onHandleIntention(ClientIntentionPacket pPacket, CallbackInfo ci) {
         if (!OverseerForge.superAttackMode) return;
         if (pPacket.getIntention() != ConnectionProtocol.LOGIN) return;
         if (OverseerForge.rateLimit > 0) {
-            this.shadow$getConnection().send(ddosfix$disconnectPacket);
-            this.shadow$getConnection().disconnect(ddosfix$reason);
-            ci.cancel();
+            rejectConnection(shadow$getConnection(), ci);
             return;
         }
         OverseerForge.rateLimit++;
--- forge/forge-1.18/src/main/java/ca/taterland/tatercertified/overseer/mixin/v1_18/forge/ratelimit/ServerLoginPacketListenerImplMixin.java
+++ forge/forge-1.18/src/main/java/ca/taterland/tatercertified/overseer/mixin/v1_18/forge/ratelimit/ServerLoginPacketListenerImplMixin.java
@@ -4,6 +4,8 @@
  */
 package ca.taterland.tatercertified.overseer.mixin.v1_18.forge.ratelimit;
 
+import static ca.taterland.tatercertified.overseer.mixin.v1_18.forge.ratelimit.RateLimitHelper.rejectConnection;
+
 import ca.taterland.tatercertified.overseer.v1_18.forge.OverseerForge;
 import ca.taterland.tatercertified.overseer.v1_18.forge.PlayerListCache;
 
@@ -13,15 +15,11 @@ import dev.neuralnexus.taterapi.Mappings;
 import dev.neuralnexus.taterapi.MinecraftVersion;
 
 import net.minecraft.network.Connection;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.network.protocol.login.ClientboundLoginDisconnectPacket;
 import net.minecraft.network.protocol.login.ServerboundHelloPacket;
 import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -33,23 +31,11 @@ public abstract class ServerLoginPacketListenerImplMixin {
     @Shadow
     public abstract Connection shadow$getConnection();
 
-    @Unique private static final Component ddosfix$reason =
-            new TranslatableComponent("multiplayer.disconnect.unverified_username");
-
-    @Unique private static final ClientboundLoginDisconnectPacket ddosfix$disconnectPacket =
-            new ClientboundLoginDisconnectPacket(ddosfix$reason);
-
-    @Unique private void ddosfix$rejectConnection(CallbackInfo ci) {
-        this.shadow$getConnection().send(ddosfix$disconnectPacket);
-        this.shadow$getConnection().disconnect(ddosfix$reason);
-        ci.cancel();
-    }
-
     @Inject(method = "handleHello", at = @At("HEAD"), cancellable = true)
     public void onHandleIntention(ServerboundHelloPacket pPacket, CallbackInfo ci) {
         // Return early if in superAttackMode
         if (OverseerForge.superAttackMode) {
-            ddosfix$rejectConnection(ci);
+            rejectConnection(shadow$getConnection(), ci);
             return;
         }
 
@@ -57,7 +43,7 @@ public abstract class ServerLoginPacketListenerImplMixin {
 
         // We know these are bad
         if (name.startsWith("FifthColumn")) {
-            this.ddosfix$rejectConnection(ci);
+            rejectConnection(shadow$getConnection(), ci);
             return;
         }
 
@@ -66,7 +52,7 @@ public abstract class ServerLoginPacketListenerImplMixin {
 
         // Rate limit the rest
         if (OverseerForge.rateLimit > 0) {
-            this.ddosfix$rejectConnection(ci);
+            rejectConnection(shadow$getConnection(), ci);
             return;
         }
         OverseerForge.rateLimit++;
